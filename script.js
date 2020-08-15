21efd087511e800e2c923cf3e9814f59dba89488
Guillef33 <guillef33@gmail.com>
2020-08-15 18:33:21 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+let img = document.querySelector('img'); 
+let b1 = document.querySelector('#b1'); 
+let b2 = document.querySelector('#b2'); 
+let b3 = document.querySelector('#b3'); 
+
+b1.addEventListener('click', () => {
+	img.src = 'img/renegade1.jpg';
+ })
+
+b2.addEventListener('click', () => {
+	img.src = 'img/renegade2.jpg';
+ })
+
+b3.addEventListener('click', () => {
+	img.src = 'img/renegade3.jpg';
+ })
+
+
+
+
+
+
+
